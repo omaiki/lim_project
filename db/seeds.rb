f801f488a2de6089d40c29a2fcd04618efd75402
Oshoke Maiki <oshokemaiki@gmail.com>
2017-04-13 20:26:24 -0700

    Add url to seed file for easy rails console testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  url_params = Hash.new
+  url_params[:page_url] = "www.google.com"
+  url_params[:page_content] = {
+title: "TestChat",
+location: 70,
+abstract: "TestChat",
+id: 1,
+user: {
+username: nil,
+first_name: nil,
+last_name: nil,
+location: nil,
+info: nil
+},
+messages: [
+{
+content: "test_content",
+abstract: "test_content",
+id: 1
+}
+]
+}
+
+new_url = Url.create(url_params)
+
