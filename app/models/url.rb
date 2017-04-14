e6799f7e16cfceb321eb25c7e26f2b6b243b28b9
Oshoke Maiki <oshokemaiki@gmail.com>
2017-04-13 21:41:47 -0700

    Add parser functionality with help of nokogiri and open-uri

--- app/models/url.rb
+++ app/models/url.rb
@@ -4,10 +4,26 @@ class Url < ApplicationRecord
 
   after_create :parse_url
 
+  require 'nokogiri'
+  require 'open-uri'
+
    def parse_url
+    doc = Nokogiri::HTML(open(self.page_url))
+    doc.css('a').each do |link|
+      self.url_contents.create(tag: 'a', content: link.content)
+    end
+
+    doc.css('h1').each do |heading|
+      self.url_contents.create(tag: 'h1', content: heading.content)
+    end
+
+    doc.css('h2').each do |heading|
+      self.url_contents.create(tag: 'h2', content: heading.content)
+    end
+
+    doc.css('h3').each do |heading|
+      self.url_contents.create(tag: 'h3', content: heading.content)
+    end
 
   end
-
-
-  # parser code will go in this model
 end
