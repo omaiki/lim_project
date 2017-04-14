3e97b50af83a8c5e0f052a770f467818803dd4ec
Oshoke Maiki <oshokemaiki@gmail.com>
2017-04-13 19:47:18 -0700

    Add CRUD actions for urls_controller

--- app/controllers/url_contents_controller.rb
+++ app/controllers/url_contents_controller.rb
@@ -1,4 +1,23 @@
 class UrlContentsController < ApplicationController
 
+  def index
+    @url_contents = UrlContent.all
+
+    render json: @url_contents
+  end
+
+  def show
+    @url_content = UrlContent.find(params[:id])
+
+    render json: @url_content
+  end
+
+
+  private
+
+  def url_content_params
+    params.require(:url_content).permit(:content, :url_id)
+  end
+
 
 end
