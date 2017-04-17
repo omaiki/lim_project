278b998b81302a16b6483ed7ed8cb67b1f4375f7
Oshoke Maiki <oshokemaiki@gmail.com>
2017-04-17 00:31:21 -0700

    Add tests for URL model validations when being created

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   let(:input_url) { Url.create(:page_url => "https://ruby-doc.org/")}
+
+  let (:invalid_url) {Url.create(:page_url => "")}
+
+  context Url do
+    it "is valid when parameters are filled" do
+      input_url.valid?
+      expect(input_url.errors[:input_url]).to be_empty
+    end
+
+    it "is invalid when missing required input parameter" do
+      invalid_url.valid?
+      expect(invalid_url.errors[:page_url]).to include("can't be blank")
+    end
+  end
 end
